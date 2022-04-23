5d768547daa756c84bbd95731c514335689ecd80
kanipiri <kanipriya2903@gmail.com>
2022-04-23 20:02:46 +0530

    new ss

--- src/org/test01/Alerts.java
+++ src/org/test01/Alerts.java
@@ -1,15 +1,7 @@
 package org.test01;
 
-<<<<<<< HEAD
 public class Alerts {
 	
-	int a ;
-
-=======
->>>>>>> fb81be36398d297019de8c9773b5707ee9a6332c
-
-
-public class Alerts {
+	int b;
 	
-	 int a;
 }
